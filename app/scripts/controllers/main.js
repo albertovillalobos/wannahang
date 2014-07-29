b7b7aded370eeda1c6678ad0fb43b1b222b48514
Isaac <alberto@albertovillalobos.me>
2014-07-29 14:39:16 -0700

    fixed the tagwords stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,30 @@
 'use strict';
 
 angular.module('wannahangApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+  .controller('MainCtrl', function ($scope, $http, $interval) {
+
+  	$scope.tagwords = [
+      "hang",
+      "chill",
+      "meet",
+      "study",
+      "hack",
+      "party",
+      "have a few beers",
+      "game",
+      "kick it"
+      ];
+  	$scope.tagword = "hang";
+
+  	var count = 0;
+
+  	$interval(function() {
+  		$scope.tagword = $scope.tagwords[count];
+  		count++;
+
+  		if (count >= $scope.tagwords.length) {
+  			count = 0;
+  		};
+
+  	}, 1500);
   });
